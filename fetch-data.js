bcae700a015bfeb3fbd6b1145dc43455e7d80fec
Praisel <praiseozoko3@gmail.com>
2025-01-21 23:01:17 +0100

    Updated fetch data js

---
+++ fetch-data.js
@@ -0,0 +1,29 @@
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById("api-data");
+
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok){
+            throw new Error("Network response was not ok");
+        }
+        const users = await response.json();
+    } catch (error) {
+        dataContainer.textContent = '';
+        console.error("Failed to load user data.");
+    }
+    dataContainer.innerHTML = '';
+    const userList = document.createElement('ul'); //create ul element
+    users.array.forEach(user => {
+        const listItem = document.createElement('li'); //create li element
+        listItem.textContent = user.name;
+        userList.append(listItem);
+    });
+    dataContainer.append(userList);
+}
+
+document.addEventListener(DOMContentLoaded, function(){
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    fetchUserData(apiUrl);
+});
